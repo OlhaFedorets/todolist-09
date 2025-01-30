19c5a848e1ba95a66546fd7dc5e4ad410cda3ba0
OlhaFedorets <fedoretsolha@gmail.com>
2025-01-30 11:24:45 +0300

    create AppWithRedux

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import { App } from './App'
+import {AppWithRedux} from "./AppWithRedux.tsx";
 
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(<AppWithRedux />)
